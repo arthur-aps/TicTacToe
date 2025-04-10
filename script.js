f5053802e9fb29e7ad070b6957f7cc01b2351a7e
Arthur Silva <amaral.psof@gmail.com>
2025-04-10 14:11:15 -0300

    Polished the code for performance

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 "use strict";
-
+// tamanho do grid // size of the grid
+const gridSize = 9;
 // todas as combinações de vitória possíveis: // all victory combinations possible:
 const combinacoes = [
-  [0,1,2], [3,4,5], [6,7,8], // linhas // lines
-  [0,3,6], [1,4,7], [2,5,8], // colunas // columns
+  [0,1,2], [3,4,5], [6,7,8], // linhas    // lines
+  [0,3,6], [1,4,7], [2,5,8], // colunas   // columns
   [0,4,8], [2,4,6]           // diagonais // diagonals
 ];
+// criando uma HTMLCollection com todas as células // creating a HTMLCollection with all the cells
+const cellCollection = document.getElementsByClassName("celula");
+const celulas = [];
+// sets que contam as jogadas de cada jogador // sets counting the players moves
+const jogadasX = new Set();
+const jogadasO = new Set();
+// consegue o elemento que irá mostrar mensagens de vitória ou empate
+// gets the element that will show victory or tie messages
+const victory = document.getElementById("victory");
 
 let jogador = "x";
 let gameEnd = false;
@@ -30,10 +40,6 @@ class Celula {
   }
 }
 
-// criando uma HTMLCollection com todas as células // creating a HTMLCollection with all the cells
-const cellCollection = document.getElementsByClassName("celula");
-const celulas = [];
-
 // criação dos objetos celulas: // creation of celulas objects:
 for (let i = 0; i < cellCollection.length; i++) {
   const id = "c" + (i + 1);
@@ -48,6 +54,7 @@ for (let i = 0; i < cellCollection.length; i++) {
   cellCollection[i].addEventListener("click", () => verificacaoDeCelula(celulaObj));
 }
 
+
 // função que verifica o estado da célula e a muda de acordo com ele, também alterando o jogador:
 // function that verifies the state of the cell and changes it, also altering the player:
 function verificacaoDeCelula(celula) {
@@ -55,71 +62,51 @@ function verificacaoDeCelula(celula) {
   if (gameEnd) return;
 
   if (celula.state == null) {
-    switch (jogador) {
-      case "x":
-        celula.state = "x";
-        jogador = "o";
-        break;
-      case "o":
-        celula.state = "o";
-        jogador = "x";
-        break;
-      default:
-        console.log(`Erro ao definir estado da célula ${celula.name}.`)
-    }
-    document.getElementById(celula.name).textContent = celula.state;
+    celula.state = jogador;
+    
+    document.getElementById(celula.name).textContent = jogador;
     console.log(`Estado da célula ${celula.name}: ${celula.state}`);
 
+    switch (jogador) {
+      case "x":
+        jogadasX.add(celula.index);
+        console.log(`jogadasX size: ${jogadasX.size}`)
+        break;
+      default:
+        jogadasO.add(celula.index);
+        console.log(`jogadasO size: ${jogadasO.size}`)
+    }
     checarVitoria();
-  }
+
+    jogador = jogador === "x" ? "o" : "x";
+  }  
 }
 
 // checa se alguém ganhou, ou houve empate // checks if someone won, or if it's a tie
 function checarVitoria() {
-  const xArray = [];
-  const oArray = [];
-  const victory = document.getElementById("victory");
-  let nullCells = 9;
-  let noWinner = true;
-
-  for (const cell of celulas) {
-    switch (cell.state) {
-      case "x":
-        xArray.push(cell.index);
-        nullCells--;
-        break;
-      case "o":
-        oArray.push(cell.index);
-        nullCells--;
-        break;
-      default:
-        continue;
-    }
-  }
-
-  // checa se os valores da array menor (combinacoes) estão na array possivelmente vencedora (x ou o)
-  // checks if the values of the smaller array (combinacoes) are in the possible winning array (x or o)
-  function contemTodos(arrMaior, arrMenor) {
-    return arrMenor.every(val => arrMaior.includes(val));
+  // checa se os valores da array menor (combinacao of combinacoes) estão no set possivelmente vencedor (jogadasX ou jogadasO)
+  // checks if the values of the smaller array (combinacoo of combinacoes) are in the possible winning set (jogadasX or jogadasO)
+  function contemTodos(setMaior, arrMenor) {
+    return arrMenor.every(val => setMaior.has(val));
   }
 
   for (const combinacao of combinacoes) {
-    if (contemTodos(xArray, combinacao)) {
-      victory.textContent = `x wins!`;
-      noWinner = false;
+    if (contemTodos(jogadasX, combinacao)) {
+      victory.textContent = "x wins!";
+      console.log("Winner: o");
       gameEnd = true;
       return;
     }
-    if (contemTodos(oArray, combinacao)) {
-      victory.textContent = `o wins!`;
-      noWinner = false;
+    if (contemTodos(jogadasO, combinacao)) {
+      victory.textContent = "o wins!";
+      console.log("Winner: o");
       gameEnd = true;
       return;
     }
   }
-
-  if (nullCells == 0 && noWinner) {
+  if (jogadasX.size + jogadasO.size === gridSize) {
     victory.textContent = "tie!"
+    console.log("No winner: it's a tie.")
     gameEnd = true;
     return;
   }
